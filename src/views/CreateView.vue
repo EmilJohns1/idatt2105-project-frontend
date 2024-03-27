5d1cc959109876acdbbdcd048b374bfbae921b45
bragetn <brage.naustan@gmail.com>
2024-03-27 13:58:41 +0100

    Improved CreateView Layout

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,43 +1,62 @@
 <template>
   <div class="sidebar">
-    <a class="active" href="#q1">Question 1</a>
-    <a href="#q2">Question 2</a>
-    <a href="#q3">Question 3</a>
-    <a href="#add">Add question</a>
+    <div id="questions">
+      <div class="questionButtonDiv" v-for="(q, index) in questions" :key="index" @mouseover="handleOver(index)" @mouseleave="handleLeave(index)">
+        <a :href="'#q' + (index + 1)" :class="{ active: index === 0 }" class="questionButton">Question {{ index + 1 }}</a>
+        <img src='../assets/Trash-White.png' v-if="isHovered[index]" @click="deleteQuestion(index)" class="deleteButton">
+      </div>
+    </div>
+  <a href="#add" @click="newQuestion()">Add question</a>
   </div>
 
   <div class="content">
-    <h2 class="header">Question</h2>
-    <input type="text" class="choiceInput" v-model="question"><br>
+
+    <header>
+  <input class="toggle-menu" id="toggle-menu" type="checkbox" />
+  <h2 class="mainHeader">Question</h2>
+  <nav>
+    <ul>
+      <li>Max Points<br>
+      <input type="number" id="maxPointsInput" min="0" max="10" v-model="maxPoints"></li>
+      <li>Category<br>
+      <input type="text" class="categoryInput" v-model="category"></li>
+      <li>Difficulty<br>
+      <input type="range" min="1" max="5" value="3" id="difficulty"></li>
+      <li><button class="blackButton">Import Question</button></li>
+      <li><button class="blackButton">Export Question</button><br></li>
+    </ul>
+  </nav>
+  <label class="button-toggle-menu" for="toggle-menu">Options</label>
+  <label class="overlay"  for="toggle-menu"></label>
+</header>
+    <input type="text" class="choiceInput"><br>
 
     <h2 class="header">Image or Video</h2>
     <img :src="imageUrl || placeholderImage" id="image" /><br>
     <input accept="image/*" type="file" @change="previewImage" /><br>
 
-    <div class="header qType">Question type
+    <div class="qType header">
+      Question type
       <select name="QuestionType" id="QuestionType" @change="handleQuestionTypeChange"> 
         <option value="mc">Multiple Choice</option> 
-        <option value="tof">Truth or False</option> 
+        <option value="tof">True or False</option> 
         <option value="fitb">Fill in the blank</option> 
       </select>
     </div>
-    <div class="header difficulty">
-      Difficulty
-      <input type="range" min="1" max="5" value="3" id="difficulty">
-    </div>
 
     <div class="choice" v-if="selectedQuestionType === 'mc'">
-      Correct answer:<input type="text" class="choiceInput"><br>
-      Wrong answer: <input type="text" class="choiceInput"><br>
-      Wrong answer: <input type="text" class="choiceInput"><br>
-      Wrong answer: <input type="text" class="choiceInput"><br>
+      Alt 1: <input type="text" class="choiceInput"> <label class="container"><input type="checkbox" checked="checked"><span class="checkmark"></span></label><br>
+      Alt 2: <input type="text" class="choiceInput"> <label class="container"><input type="checkbox"><span class="checkmark"></span></label><br>
+      Alt 3: <input type="text" class="choiceInput"> <label class="container"><input type="checkbox"><span class="checkmark"></span></label><br>
+      Alt 4: <input type="text" class="choiceInput"> <label class="container"><input type="checkbox"><span class="checkmark"></span></label><br>
     </div>
     <div class="choice" id="tof" v-if="selectedQuestionType === 'tof'">
-      Truth <input type="radio" name="tof"><br>
-      False <input type="radio" name="tof"><br>
+      Answer:<br>
+      True&nbsp; <label class="container"><input type="radio" name="tof" checked><span class="checkmark"></span></label><br>
+      False <label class="container"><input type="radio" name="tof"><span class="checkmark"></span></label><br>
     </div>
     <div class="choice" v-if="selectedQuestionType === 'fitb'">
-      Correct answer: <input type="text" class="choiceInput">
+      Answer: <input type="text" class="choiceInput">
     </div>
   </div>
 </template>
@@ -46,10 +65,16 @@
 export default {
   data() {
     return {
+      questions: [{ question: '', imageUrl: null, maxPoints: 1, category: '', selectedQuestionType: 'mc' }],
+      currentQuestionIndex: 0,
       question: '',
       imageUrl: null,
       placeholderImage: 'src/assets/placeholder-image.jpg',
-      selectedQuestionType: 'mc' // Default selection
+      selectedQuestionType: 'mc',
+      maxPoints: '1', 
+      category: '',
+      questionQuantity: 1,
+      isHovered: []
     };
   },
   methods: {
@@ -63,16 +88,53 @@ export default {
     },
     handleQuestionTypeChange(event) {
       this.selectedQuestionType = event.target.value;
+    },
+    newQuestion() {
+      this.questions.push({ question: '', imageUrl: null, maxPoints: 1, category: '', selectedQuestionType: 'mc' });
+      this.currentQuestionIndex = this.questions.length - 1;
+    },
+    handleLeave(index){
+      this.isHovered[index] = false;
+    },
+    handleOver(index){
+      this.isHovered[index] = true;
+    },
+    deleteQuestion(index) {
+      console.log(index)
+      this.questions.splice(index, 1);
+      this.isHovered.splice(index, 1);
     }
   }
 };
 </script>
 
+
   
   <style scoped>
-
+  .deleteButton{
+    position: absolute;
+    margin-left: 85%;
+    margin-top: 12px;
+    cursor:pointer;
+    border: none;
+    background: none;
+  }
+  .deleteButton:hover{
+    content:url("../assets/Trash-Redder.png")
+  }
+  .questionButton{
+    position: absolute;
+    width:100%
+  }
+  .questionButtonDiv{
+    position: relative;
+    height: 55px;
+  }
+  .mainHeader{
+    margin-left: 37%;
+  }
   .choiceInput{
-    width:70%
+    width:50%
   }
   #tof{
     margin-left: 22%;
@@ -81,16 +143,8 @@ export default {
     text-align: left;
     width: 100%;
     margin-top: 100px;
-    margin-left: 15%;
+    margin-left: 21%;
     font-size: 20px;
-  }
-  .qType{
-    float: left;
-    margin-left: 15%;
-  }
-  .difficulty{
-    float: right;
-    margin-right: 15%;
   }
   .header{
     margin-top: 40px;
@@ -134,23 +188,159 @@ export default {
     margin-left: 20%;
     padding: 1px 16px;
     height: 1000px;
+    overflow-x: hidden;
   }
   
-  @media screen and (max-width: 700px) {
-    .sidebar {
-      width: 100%;
-      height: auto;
-      position: relative;
-    }
-    .sidebar a {float: left;}
-    .content {margin-left: 0;}
+  .maxPoints,
+  .category {
+    font-size: 20px;
+    display: inline-block;
+    margin-top: 60px;
+    margin-right: 13%; 
+    margin-left: 13%;
   }
+
+  .qType {
+    clear: both; 
+    float: left;
+    margin-left: 20%;
+    margin-top: 40px; 
+  }
+
+  .difficulty {
+    float: right;
+    margin-right: 15%;
+    margin-top: 40px; 
+  }
+
+  #maxPointsInput {
+    width: 60px;
+  }
+
+  .categoryInput {
+    width: 150px;
+  }
+
+  header {
+  height: 50px;
+  width: 100%;
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.button-toggle-menu {
+  border-radius: 10%;
+  cursor: pointer;
+  font-size: 20px;
+  background-color: #d1cece;
+  margin-top: 300px;
+  padding: 10px 20px;
+}
+
+.toggle-menu {
+  appearance: none;
+
+  &:checked {
+    ~ nav {
+      transform: translateX(90%) translateY(105%);;
+    }
+  }
+}
+
+nav {
+  position:absolute, 0 null 0 0;
+  background: #ffffff;
+  width: 300px;
+  transform: translateX(500%) translateY(100%);
+  will-change: transform;
+  transition: .3s transform linear;
+  z-index: 1;
+  border-left: solid black 1px;
   
-  @media screen and (max-width: 400px) {
-    .sidebar a {
-      text-align: center;
-      float: none;
+  ul {
+    list-style: none;
+    padding: 20px 0;
+    margin: 0;
+    height: 100%;
+    
+    li {
+      padding: 10px 20px;
     }
   }
+}
+
+.container {
+  display: inline-block;
+  position: relative;
+  cursor: pointer;
+}
+
+.container input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+
+.checkmark {
+  position: absolute;
+  top: -15px;
+  left: 0;
+  height: 20px;
+  width: 20px;
+  background-color: rgb(206, 203, 203);
+  border-radius: 10%;
+  border: 1px solid #000;;
+}
+
+.container input:checked ~ .checkmark {
+  background-color: Green;
+}
+
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+.container input:checked ~ .checkmark:after {
+  display: block;
+}
+
+.container .checkmark:after {
+  left: 6px;
+  top: 2px;
+  width: 7px;
+  height: 11px;
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
+
+/* for slim display */
+@media only screen and (max-width: 700px){
+  .sidebar {
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 20%;
+  background-color: #f1f1f1;
+  overflow: auto;
+  border-bottom: solid 1px black;
+}
+  .questionButton{
+    width:100%
+  }
+  .questionButtonDiv{
+    width:25%
+  }
+
+}
   </style>
   
