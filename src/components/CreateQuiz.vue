e5d5be93db0ffab45793e55a936cf638ea5bb84c
bragetn <brage.naustan@gmail.com>
2024-03-30 16:23:04 +0100

    Made CreateQuiz functional

--- src/components/CreateQuiz.vue
+++ src/components/CreateQuiz.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
       <h1 id="header">Create new quiz</h1>
-      <form @submit.prevent="">
+      <form @submit.prevent="submitForm">
         <h2>Title</h2>
         <input v-model="title" type="text" required class="input-field" />
         <h2>Description</h2>
@@ -10,16 +10,16 @@
         <img :src="imageUrl || placeholderImage" id="image" /><br>
         <input accept="image/*" type="file" @change="previewImage" /><br>
         <h3>Category</h3>
-        <select name="category" id="category" > 
-        <option value="mc">test 1</option> 
-        <option value="tof">test 2</option> 
-        <option value="fitb">test 3</option> 
-      </select>
+        <select v-model="category">
+      <option disabled value="">Select a category</option>
+      <option v-for="cat in categories" :key="cat" :value="cat">{{ cat }}</option>
+    </select>
         <h3>Add tags</h3>
         <div class="tags-input"> 
       <ul id="tags"></ul> 
       <input type="text" id="input-tag" 
           placeholder="Enter tag (e.g. difficult)" /> 
+          <button type="button" @click="addTagElement" :disabled="tagArray.length > 2" id="addTagButton">Create tag</button>
   </div> 
         <div class="button-container">
           <button type="submit" class="submit-button">Create quiz</button>
@@ -30,21 +30,106 @@
   
   <script setup lang="ts">
   import { ref, onMounted } from 'vue';
+  import router from '@/router';
+  import { useRegistration, getCategories, addUserToQuiz,  addTagsToQuiz } from '@/api/quizHooks'
+  import type { QuizRequest } from '@/types/QuizRequest';
+  import type { Tag } from '@/types/Tag';
+  import {getUserByUsername} from '@/api/userHooks'
   
-  const imageUrl = ref<string | null>(null);
+  const imageUrl = ref('');
   const placeholderImage = 'src/assets/placeholder-image.jpg';
   const title = ref('');
   const description = ref('');
+  const category = ref('');
+  const tagArray = ref<string[]>([]);
+
+  const { registerQuiz, clearError } = useRegistration()
+  const registrationError = ref('')
+
+  const categories = ref<string[] | null>(null); 
+
+  //fetch categories
+  const fetchCategories = async () => {
+    categories.value = await getCategories(); 
+  }
+
+  onMounted(fetchCategories)
+
+  const submitForm = async () => {
+  clearError()
+
+  //registering quiz
+  const quizData: QuizRequest = {
+    title: title.value,
+    description: description.value,
+    quizPictureUrl: imageUrl.value,
+    categoryName: category.value
+  }
+  console.log(quizData)
+  const quizId = await registerQuiz(quizData)
+  if (quizId ==null) {
+    registrationError.value = 'Registration failed.'
+  } else {
+    redirect(quizId,quizData.title)
+  }
+
+
+  if (tagArray.value && quizId) {
+    let tagsData: Tag[] = []; 
+    for (let i = tagArray.value.length - 1; i >= 0; i--) {
+        const tag: Tag = { tagName: tagArray.value[i] };
+        tagsData.push(tag);
+    }
+    console.log(tagsData)
+    addTagsToQuiz(tagsData, quizId);
+}
+
+  const username = sessionStorage.getItem('user')
+  if (username && quizId) {
+    try {
+      const userData = await getUserByUsername(username)
+      if (userData) {
+        const userId = userData.id
+        addUserToQuiz(quizId, userId)
+      }
+    }catch{
+      console.log("Failed to add user to quiz")
+    }
+
+}}
+
+
+const redirect = (quiz_id: number,quiz_title: string) => {
+  router.push('/quiz/{'+quiz_id+'}-{'+quiz_title+'}/edit')
+  setTimeout(() => {
+    setTimeout(() => {
+      window.scrollTo(0, 0)
+    }, 0)
+  }, 250)
+}
+
+const addTagElement = () => {
+  const tag = document.createElement('li');
+  const tagContent = input.value!.value.trim();
+
+  if (tagContent !== '') {
+    console.log(tagArray.value.length)
+    tagArray.value.push(tagContent);
+    tag.innerText = tagContent;
+    tag.innerHTML += '<button class="delete-button">X</button>';
+    tags.value?.appendChild(tag);
+    input.value!.value = '';
+  }
+}
   
   const previewImage = (event: Event) => {
     const file = (event.target as HTMLInputElement).files?.[0];
     if (file) {
       imageUrl.value = URL.createObjectURL(file);
     } else {
-      imageUrl.value = null;
+      imageUrl.value = '';
     }
   };
-
 
   const tags = ref<HTMLElement | null>(null);
   const input = ref<HTMLInputElement | null>(null);
@@ -57,28 +142,21 @@ onMounted(() => {
     throw new Error('Could not find tags or input elements in the DOM');
   }
 
-  input.value.addEventListener('keydown', (event: KeyboardEvent) => {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-
-      const tag = document.createElement('li');
-      const tagContent = input.value!.value.trim();
-
-      if (tagContent !== '') {
-        tag.innerText = tagContent;
-        tag.innerHTML += '<button class="delete-button">X</button>';
-        tags.value?.appendChild(tag);
-        input.value!.value = '';
-      }
-    }
-  });
-
   tags.value.addEventListener('click', (event: MouseEvent) => {
     const target = event.target as HTMLElement;
-    if (target.classList.contains('delete-button')) {
-      const parent = target.parentNode as HTMLElement;
-      parent.removeChild(target); // remove the button itself
-      parent.remove(); // remove the whole tag
+  if (target.classList.contains('delete-button')) {
+    const parent = target.parentNode as HTMLElement;
+    const tagContent = parent.textContent?.trim().replace('X', ''); 
+
+    if (tagContent) {
+      const index = tagArray.value.findIndex(tag => tag === tagContent);
+
+      if (index !== -1) {
+        tagArray.value.splice(index, 1); 
+      }
+    }
+      parent.removeChild(target); 
+      parent.remove(); 
     }
   });
 });
@@ -87,6 +165,30 @@ onMounted(() => {
   
 
 <style scoped>
+.tags-input{
+  height: 100px;
+}
+#input-tag{
+  position: absolute;
+    bottom: 0;
+    left:0;
+}
+#addTagButton{
+  position: absolute;
+    bottom: 0;
+    right:0;
+  width:20%;
+  font-size: 15px;
+  color:white;
+  border-radius: 10%;
+  background-color: black;
+  border: 0px;
+  cursor: pointer;
+}
+#addTagButton:disabled{
+  background-color: rgb(143, 143, 143);
+  cursor:default
+}
 #category{
   min-width: 150px;
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,10 +3,12 @@ import SignUpView from '../views/SignUpView.vue'
 import LoginView from '../views/LoginView.vue'
 import ExploreView from '../views/ExploreView.vue'
 import SubjectView from '../views/SubjectView.vue'
-import CreateView from '@/views/CreateView.vue'
 import UserPageView from '@/views/UserPageView.vue'
 import ContactView from '@/views/ContactView.vue'
 import ResetPasswordView from '@/views/ResetPasswordView.vue'
+import CreateQuizView from '../views/CreateQuizView.vue'
+import EditView from '../views/CreateView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,9 +25,14 @@ const router = createRouter({
       props: true
     },
     {
+      path: '/editQuiz',
+      name: 'editQuiz',
+      component: EditView
+    },
+    {
       path: '/create',
       name: 'create',
-      component: CreateView
+      component: CreateQuizView
     },
     {
       path: '/about',
