d7c9cb9920e27867c38c46ebeac6ebb38ef4dadd
Svein Kåre <sveiks@stud.ntnu.no>
2024-04-01 20:41:25 +0200

    Fixed logout race condition

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -58,14 +58,13 @@ watch(isLoggedIn, fetchUserProfilePicture);
 const logout = () => {
   oauth2.post('/oauth2/revoke', {
     token: userStore.getAccessToken
-  }).then(() => {
-    userStore.logout()
-    sessionStorage.clear()
   })
   const clientId = apiStore.clientId
   const authUrl =
     apiStore.getBackendUrl +
     `/connect/logout?client_id=${clientId}&id_token_hint=${userStore.getIdToken}&post_logout_redirect_uri=${apiStore.getBaseUrl}`
+  userStore.logout()
+  sessionStorage.clear()
   window.location.href = authUrl
 }
 
