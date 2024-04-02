6243caa84a2ac3dc43e97105c0120cdbd18ca992
SveinKare <sksorestad@gmail.com>
2024-04-02 14:03:41 +0200

    Moved api call logic to separate authentication hook

--- src/api/authenticationHooks.ts
+++ src/api/authenticationHooks.ts
@@ -1,4 +1,6 @@
 import { useApiStore } from '@/stores/apiStore'
+import { oauth2 } from '@/api/axiosConfig'
+import { useUserStore } from '@/stores/userStore'
 
 export const useLogin = async () => {
   const apiStore = useApiStore()
@@ -15,6 +17,60 @@ export const useLogin = async () => {
     `/oauth2/authorize?client_id=${clientId}&response_type=${responseType}&scope=${scope}&redirect_uri=${redirectUri}&code_challenge=${codeChallenge}&code_challenge_method=S256`
   window.location.href = authUrl
 }
+
+export const getTokens = async (code: string) => {
+  const apiStore = useApiStore()
+  const userStore = useUserStore()
+
+  const clientId = apiStore.getClientId
+  const redirectUri = apiStore.getBaseUrl + '/token'
+
+  //Get tokens
+  const tokenRes = await oauth2.post(
+    '/oauth2/token', 
+    new URLSearchParams({
+      code: code,
+      redirect_uri: redirectUri,
+      grant_type: 'authorization_code',
+      client_id: clientId,
+      code_verifier: apiStore.getCodeVerifier
+    }).toString(), 
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    }
+  )
+  if (tokenRes.status === 200) {
+    try { //Try catch here due to the possibility of the response not being JSON
+      userStore.setAccessToken(tokenRes.data.access_token)
+      userStore.setIdToken(tokenRes.data.id_token)
+    } catch (error) {
+      console.error('Failed to parse tokens:', error)
+      return
+    }
+  } else {
+    console.error('Failed to get tokens:', tokenRes.data)
+    return
+  }
+
+  //Get username
+  const userInfoRes = await oauth2.post('/userinfo', null, {
+    headers: {
+      Authorization: `Bearer ${userStore.getAccessToken}`
+    }
+  })
+
+  if (userInfoRes.status === 200) {
+    userStore.setUserName(userInfoRes.data.sub)
+    //Other info can also be gotten here
+  } else {
+    console.error('Failed to get user info:', userInfoRes.data)
+    return    
+  }
+}
+
+
 
 function base64URLEncode(arrayBuffer: ArrayBuffer) {
   return btoa(String.fromCharCode.apply(null, Array.from(new Uint8Array(arrayBuffer))))
--- src/views/TokenHandlerView.vue
+++ src/views/TokenHandlerView.vue
@@ -8,68 +8,30 @@
 
 <script setup>
 import { onMounted } from 'vue'
-import { useApiStore } from '@/stores/apiStore'
-import { useUserStore } from '@/stores/userStore'
 import { useRoute, useRouter } from 'vue-router'
-import { oauth2 } from '@/api/axiosConfig'
+import { getTokens } from '@/api/authenticationHooks'
 
 /**
  * When this component is mounted, it will extract the code parameter from the URL and use it to get an access token,
  * which it in turn uses to get the user's information.
  */
 onMounted(() => {
-  const apiStore = useApiStore()
-  const userStore = useUserStore()
   const route = useRoute()
   const router = useRouter()
 
   const code = route.query.code
   if (!code) {
     // If there is no code parameter, redirect to home page
+    console.error('No code parameter found in URL')
     router.push('/')
     return
   }
-  const clientId = apiStore.clientId
-  const backend = apiStore.getBackendUrl + '/oauth2/token'
-  const redirectUri = apiStore.getBaseUrl + '/token'
-
-  oauth2
-    .post(
-      '/oauth2/token',
-      new URLSearchParams({
-        //Gets an access token
-        code: code,
-        redirect_uri: redirectUri,
-        grant_type: 'authorization_code',
-        client_id: clientId,
-        code_verifier: apiStore.getCodeVerifier
-      }).toString(),
-      {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      }
-    )
-    .then((response) => {
-      userStore.setAccessToken(response.data.access_token)
-      userStore.setIdToken(response.data.id_token)
-      oauth2
-        .post('/userinfo', null, {
-          //Gets the user's information
-          headers: {
-            Authorization: 'Bearer ' + response.data.access_token
-          }
-        })
-        .then((response) => {
-          userStore.setUserName(response.data.sub)
-        })
-        .catch((error) => {
-          console.error(error)
-        })
+  
+  getTokens(code)
+    .then(() => router.push('/'))
+    .catch(() => {
+      console.error('Failed to get tokens, returning to main menu')
       router.push('/')
-    })
-    .catch((error) => {
-      console.error(error)
     })
 })
 </script>
