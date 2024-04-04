cc9850a7697b663f26edd216da98728c156a0075
SveinKare <sksorestad@gmail.com>
2024-04-04 16:04:46 +0200

    Basic layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,28 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div class="discover">
+      <h1>Discover</h1>
+      <div class="category-cards">
+
+      </div>
+    </div>
+    <div class="login">
+      <h1>Or Create?</h1>
+      <div class="buttons">
+        <RouterLink to="/signup" class="signup-button">Sign up</RouterLink>
+        <RouterLink to="/login" class="login-button">Log in</RouterLink>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
 
-const message = ref('Hello, World!')
+
+
+
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
