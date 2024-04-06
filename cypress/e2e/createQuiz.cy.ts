18def38f57e71919d0aea715f55b35afc07ef905
SveinKare <sksorestad@gmail.com>
2024-04-06 13:45:51 +0200

    Added some e2e tests

---
+++ cypress/e2e/createQuiz.cy.ts
@@ -0,0 +1,28 @@
+
+
+describe('CreateQuiz Component', () => {
+    beforeEach(() => {
+      // Assuming your app runs on localhost:3000
+      cy.visit('https://localhost:4173/quiz/create')
+    })
+  
+    it('should render the form', () => {
+      cy.get('form').should('be.visible')
+    })
+  
+    it('should allow user to input quiz title', () => {
+      cy.get('input[class="input-field"]').type('Test Quiz')
+      cy.get('input[class="input-field"]').should('have.value', 'Test Quiz')
+    })
+  
+    it('should allow user to input quiz description', () => {
+      cy.get('textarea[class="input-field description"]').type('This is a test quiz')
+      cy.get('textarea[class="input-field description"]').should('have.value', 'This is a test quiz')
+    })
+  
+    it('should allow user to add tags', () => {
+      cy.get('input[id="input-tag"]').type('tag1')
+      cy.get('button').contains('Create tag').click()
+      cy.get('ul[id="tags"]').should('contain', 'tag1')
+    })
+  })
---
+++ cypress/e2e/navbar.cy.ts
@@ -0,0 +1,38 @@
+
+
+describe('NavBar Component', () => {
+  
+    beforeEach(() => {
+      cy.visit('https://localhost:4173')
+    })
+  
+    it('should navigate to home page when logo is clicked', () => {
+      cy.get('#logo').click()
+      cy.url().should('include', '/')
+    })
+  
+    it('should navigate to explore page', () => {
+      cy.get('a.link').contains('Explore').click()
+      cy.url().should('include', '/explore')
+    })
+  
+    it('should navigate to create quiz page', () => {
+      cy.get('a.link').contains('Create').click()
+      cy.url().should('include', '/quiz/create')
+    })
+  
+    it('should navigate to contact page', () => {
+      cy.get('a.link').contains('Contact').click()
+      cy.url().should('include', '/contact')
+    })
+  
+    it('should navigate to FAQ page', () => {
+      cy.get('a.link').contains('FAQ').click()
+      cy.url().should('include', '/faq')
+    })
+  
+    it('should navigate to signup page', () => {
+      cy.get('a.purpleButton').contains('Sign up').click()
+      cy.url().should('include', '/signup')
+    })
+  })
